0ca3f18d7ee1912cd86c3e8108099da401f1f574
Rob Rivas <rrivas@goldstar.com>
2014-03-31 19:07:01 -0700

    move all the configuration parts to a different file

---
+++ config.rb
@@ -0,0 +1,10 @@
+require 'rubygems'
+require 'action_mailer'
+require 'mysql2'
+require 'active_record'
+
+@environment = ENV['RACK_ENV'] || 'development'
+@dbconfig = YAML.load(File.read('config/database.yml'))
+ActiveRecord::Base.establish_connection @dbconfig[@environment]
+
+Dir['mailers/*.rb'].each { |file| require_relative file }
