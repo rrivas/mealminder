af441f14173c0ce7931a9cecae0fae9658315fbb
Rob Rivas <rrivas@goldstar.com>
2014-03-31 18:28:13 -0700

    Add sqlite3 database

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source "https://rubygems.org"
 
 gem 'actionmailer'
 gem 'rspec'
+gem 'activerecord'
+gem 'sqlite3'
--- mealminder.rb
+++ mealminder.rb
@@ -1,5 +1,12 @@
 require 'rubygems'
 require 'action_mailer'
+require 'sqlite3'
+require 'active_record'
+
+ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database  => "db/database.sqlite3"
+)
 
 Dir['mailers/*.rb'].each { |file| require_relative file }
 
